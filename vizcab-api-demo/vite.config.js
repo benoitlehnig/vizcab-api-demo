3b7f607c88d306324cd12903d0b30bfc50a93464
benoitlehnig <bl@vizcab.io>
2025-03-12 18:11:37 +0100

    testing gh page

--- vizcab-api-demo/vite.config.js
+++ vizcab-api-demo/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [vue()],
   build: {
     outDir: 'dist',
     sourcemap: true,
   },
-  base: command === 'serve' ? '/' : '/vizcab-api-demo/', // ✅ Dynamic base path
-}));
+  base: './', // ✅ Fixes asset paths for GitHub Pages & Local Build
+});
